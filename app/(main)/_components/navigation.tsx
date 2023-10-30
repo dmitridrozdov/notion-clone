3a5047594e62b185e5d7b809f7ec93ead6d9ac71
dmitridrozdov <dm.drozdov@gmail.com>
2023-10-30 16:21:37 +1100

    Update navigation.tsx

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -46,19 +46,19 @@ export const Navigation = () => {
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
-//   useEffect(() => {
-//     if (isMobile) {
-//       collapse();
-//     } else {
-//       resetWidth();
-//     }
-//   }, [isMobile]);
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    } else {
+      resetWidth();
+    }
+  }, [isMobile]);
 
-//   useEffect(() => {
-//     if (isMobile) {
-//       collapse();
-//     }
-//   }, [pathname, isMobile]);
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    }
+  }, [pathname, isMobile]);
 
   const handleMouseDown = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -91,35 +91,35 @@ export const Navigation = () => {
     document.removeEventListener("mouseup", handleMouseUp);
   };
 
-//   const resetWidth = () => {
-//     if (sidebarRef.current && navbarRef.current) {
-//       setIsCollapsed(false);
-//       setIsResetting(true);
+  const resetWidth = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(true);
 
-//       sidebarRef.current.style.width = isMobile ? "100%" : "240px";
-//       navbarRef.current.style.setProperty(
-//         "width",
-//         isMobile ? "0" : "calc(100% - 240px)"
-//       );
-//       navbarRef.current.style.setProperty(
-//         "left",
-//         isMobile ? "100%" : "240px"
-//       );
-//       setTimeout(() => setIsResetting(false), 300);
-//     }
-//   };
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+      navbarRef.current.style.setProperty(
+        "width",
+        isMobile ? "0" : "calc(100% - 240px)"
+      );
+      navbarRef.current.style.setProperty(
+        "left",
+        isMobile ? "100%" : "240px"
+      );
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  };
 
-//   const collapse = () => {
-//     if (sidebarRef.current && navbarRef.current) {
-//       setIsCollapsed(true);
-//       setIsResetting(true);
+  const collapse = () => {
+    if (sidebarRef.current && navbarRef.current) {
+      setIsCollapsed(true);
+      setIsResetting(true);
 
-//       sidebarRef.current.style.width = "0";
-//       navbarRef.current.style.setProperty("width", "100%");
-//       navbarRef.current.style.setProperty("left", "0");
-//       setTimeout(() => setIsResetting(false), 300);
-//     }
-//   }
+      sidebarRef.current.style.width = "0";
+      navbarRef.current.style.setProperty("width", "100%");
+      navbarRef.current.style.setProperty("left", "0");
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  }
 
 //   const handleCreate = () => {
 //     const promise = create({ title: "Untitled" })
